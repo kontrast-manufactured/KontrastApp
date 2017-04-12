9f658e390e14e090e5e43e136eb85053db3bea30
Mona subaih <m.subaih@rozn.org>
2017-04-12 15:59:52 +0300

    no message

--- index.php
+++ index.php
@@ -1,31 +1,116 @@
 <?php
-  require 'lib/phpmailer/PHPMailerAutoload.php';
+error_reporting(0);
+include 'restRequest.php';
+
 
 
 $data = json_decode(file_get_contents('php://input'), true);
-//api for youtube
-/*/*
+
+//prepare line item for XML 
+$lineItemString = '';
+$mediaFileTag = '';
+$lineItemsArr = $data['line_items'];
+//fixed Params
+$restRequest = new restRequest();
+$mainURL = "https://2393be58496d0027c70ac6b8b354130b:5845b00c86160822c624e00ab90dc5b7@kontrast-manufactured-art.myshopify.com/admin/variants/";
+$params = ".json";
+
+$restRequest->setVerb('GET');
+$restRequest->setCustomeCurlParams(array(
+    'CURLOPT_SSL_VERIFYPEER' => false,
+    'CURLOPT_SSL_VERIFYHOST' => 2
+));
+$i = 0;
+foreach ($lineItemsArr as $key => $variant) {
+    $i = $i + 1;
+    ////////////// get the product image /////////////////////
+    $productURL = "https://2393be58496d0027c70ac6b8b354130b:5845b00c86160822c624e00ab90dc5b7@kontrast-manufactured-art.myshopify.com/admin/products/";
+    $paramsvarient = $variant['product_id'] . ".json";
+
+    $restRequest->setUrl($productURL . $paramsvarient);
+
+    $restRequest->execute();
+    $productInfo = json_decode($restRequest->getResponseBody(), true);
+    $product = $productInfo['product'];
+
+    //This request for get varient sku//////////////////////////////////
+    $restRequest->setUrl($mainURL . $variant['variant_id'] . $params);
+    $restRequest->execute();
+    $varientInfo = json_decode($restRequest->getResponseBody(), true);
+
+    $lineItemString .= '<LineItem>
+          <Pages>
+            <Page>
+              <PageNumber>1</PageNumber>
+              <TemplateID>-1</TemplateID>
+              <PageProperties/>
+              <Regions>
+                <Region>
+                  <MediaID>' . $i . '</MediaID>
+                  <RegionType>IMAGE</RegionType>
+                  <RegionID>0</RegionID>
+                </Region>
+              </Regions>
+            </Page>
+          </Pages>
+          <LineNumber>' . $i . '</LineNumber>
+          <PaymentInfoID>2</PaymentInfoID>
+          <ProductCode>' . $varientInfo['variant']['sku'] . '</ProductCode>
+          <Quantity>' . $variant['quantity'] . '</Quantity>
+          <UnitPrice>' . $variant['price'] . '</UnitPrice>
+          <BaseUnitPrice>' . $variant['price'] . '</BaseUnitPrice>
+          <Tax>' . $variant['tax_lines']['price'] . '</Tax>
+          <ShippingPrice>' . $data['shipping_lines']['price'] . '</ShippingPrice>
+          <HandlingPrice>0.0</HandlingPrice>
+          <ChildLineItems/>
+        </LineItem>';
+
+    $mediaFileTag .='<MediaFile>
+      <MediaID>' . $i . '</MediaID>
+      <UploadBatchID/>
+      <SourceType>URL</SourceType>
+      <MediaFileName>' . $i . $data['name'] . '.jpg</MediaFileName>
+      <MediaFileType>IMAGE</MediaFileType>
+      <Source>
+        ' . $product['image']['src'] . '
+      </Source>
+      <RelativeSource/>
+      <Rotation>0</Rotation>
+      <SourceWidth>0</SourceWidth>
+      <SourceHeight>0</SourceHeight>
+      <FileDate>0001-01-01T00:00:00</FileDate>
+      <MediaFileProperties/>
+    </MediaFile>';
+
+
+    
+}
 $prepareData = '<?xml version="1.0" encoding="utf-8"?>
 <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
   <soap12:Body>
     <OrderSubmit xmlns="FES.DigitalIntegrationServices">
       <AppKey>11C7-2462-6CAA-4D87</AppKey>
       <OrderManifest>
-  <OriginatorOrderID>'.$data['id'].'</OriginatorOrderID>
+  <OriginatorOrderID>' . $data['name'] . '</OriginatorOrderID>
   <Summary>
-    <TotalTax>1.25</TotalTax>
-    <TotalPrice>3.25</TotalPrice>
-    <TotalItemPrice>0.232</TotalItemPrice>
-    <TotalShipping>15.44</TotalShipping>
+    <TotalTax>' . $data['total_tax'] . '</TotalTax>
+    <TotalPrice>' . $data['total_price'] . '</TotalPrice>
+    <TotalItemPrice>' . $data['total_line_items_price'] . '</TotalItemPrice>
+    <TotalShipping>' . $data['shipping_lines']['price'] . '</TotalShipping>
     <TotalHandling>0.0</TotalHandling>
     <OwnerInfo>
-      <FirstName>Fuji Testorder</FirstName>
-      <LastName>Do Not Fulfill</LastName>
-      <Phone>5555555555</Phone>
+      <FirstName>' . $data['customer']['first_name'] . '</FirstName>
+      <LastName>' . $data['customer']['last_name'] . '</LastName>
+      <Phone>' . $data['customer']['phone'] . '</Phone>
+      <Email>' . $data['customer']['email'] . '</Email>
       <OrderUserTypeID>0</OrderUserTypeID>
     </OwnerInfo>
     <TaxList>
-        <Amount>0.12</Amount>
+        <Tax>
+          <TaxID>VAT</TaxID>
+          <Rate>' . $data['tax_lines']['rate'] . '</Rate>
+          <Amount>' . $data['tax_lines']['price'] . '</Amount>
+        </Tax>
     </TaxList>
   </Summary>
   <OriginatorName>Order Integration Service 1.1</OriginatorName>
@@ -40,108 +125,46 @@ $prepareData = '<?xml version="1.0" encoding="utf-8"?>
         <TotalShipping>0.0</TotalShipping>
         <TotalHandling>0</TotalHandling>
         <OwnerInfo>
-          <FirstName>Fuji Testorder</FirstName>
-          <LastName>Do Not Fulfill</LastName>
-          <Phone>5555555555</Phone>
+          <FirstName>' . $data['customer']['first_name'] . '</FirstName>
+          <LastName>' . $data['customer']['last_name'] . '</LastName>
+          <Phone>' . $data['customer']['phone'] . '</Phone>
           <OrderUserTypeID>0</OrderUserTypeID>
         </OwnerInfo>
       </Summary>
-      <OriginatorSubOrderID>'.$data['id'].'-Sub'.'</OriginatorSubOrderID>
-      <CreateDate>2012-08-31T13:40:56-04:00</CreateDate>
+      <OriginatorSubOrderID>' . $data['name'] . '-Sub' . '</OriginatorSubOrderID>
+      <CreateDate>' . $data['created_at'] . '</CreateDate>
       <FulfillerID>30242</FulfillerID>
       <LineItems>
-        <LineItem>
-          <Pages>
-            <Page>
-              <PageNumber>1</PageNumber>
-              <TemplateID>-1</TemplateID>
-              <PageProperties/>
-              <Regions>
-                <Region>
-                  <MediaID>1</MediaID>
-                  <RegionProperties>
-                    <Property>
-                      <Name>AutoRedeye</Name>
-                      <Type>enh</Type>
-                      <Value>0</Value>
-                    </Property>
-                    <Property>
-                      <Name>Enhance</Name>
-                      <Type>enh</Type>
-                      <Value>0</Value>
-                    </Property>
-                  </RegionProperties>
-                  <RegionType>IMAGE</RegionType>
-                  <RegionID>0</RegionID>
-                  <TemplateRegionPosition>
-                      <InsetX>100</InsetX>
-                      <InsetY>100</InsetY>
-                      <Width>100</Width>
-                      <Height>100</Height>
-                      <ZIndex>0</ZIndex>
-                      
-                  </TemplateRegionPosition>
-                </Region>
-              </Regions>
-            </Page>
-          </Pages>
-          <LineNumber>1</LineNumber>
-          <PaymentInfoID>2</PaymentInfoID>
-          <ItemType>1</ItemType>
-          <ProductCode>PRGift;4075</ProductCode>
-          <Quantity>1</Quantity>
-          <UnitPrice>0</UnitPrice>
-          <BaseUnitPrice>0</BaseUnitPrice>
-          <Tax>0.1</Tax>
-          <ShippingPrice>0</ShippingPrice>
-          <HandlingPrice>0</HandlingPrice>
-          <ItemProperties>
-            <Property>
-              <Name>CropMode</Name>
-              <Value>FITIN</Value>
-            </Property>
-            <Property>
-              <Name>SurfaceID</Name>
-              <Type>OPINSTRUCTION</Type>
-              <Value>9</Value>
-            </Property>
-          </ItemProperties>
-          <ChildLineItems/>
-        </LineItem>
+        '.$lineItemString.'
       </LineItems>
       <ShippingInfo>
-        <FirstName>'.$data['customer']['first_name'].'</FirstName>
-        <LastName>'.$data['customer']['last_name'].'</LastName>
-        <Address1>'.$data['customer']['email'].'</Address1>
-        <Address2>'.$data['customer']['email'].'</Address2>
-        <Address3>'.$data['customer']['email'].'</Address3>
-        <City>'.$data['customer']['email'].'</City>
-        <State>'.$data['customer']['email'].'</State>
-        <PostalCode>'.$data['customer']['email'].'</PostalCode>
-        <Country>US</Country>
-        <Phone>'.$data['customer']['email'].'</Phone>
-        <Email/>
-        <PickupTime>2012-08-27T11:00:00</PickupTime>
+        <FirstName>' . $data['shipping_address']['first_name'] . '</FirstName>
+        <LastName>' . $data['shipping_address']['last_name'] . '</LastName>
+        <Address1>' . $data['shipping_address']['address1'] . '</Address1>
+        <Address2>' . $data['shipping_address']['address2'] . '</Address2>
+        <City>' . $data['shipping_address']['city'] . '</City>
+        <State>' . $data['shipping_address']['Kentucky'] . '</State>
+        <PostalCode>' . $data['shipping_address']['zip'] . '</PostalCode>
+        <Country>' . $data['shipping_address']['country'] . '</Country>
+        <Phone>' . $data['shipping_address']['phone'] . '</Phone>
         <MethodCode>SD</MethodCode>
         <MethodName>Standard Delivery</MethodName>
       </ShippingInfo>
       <Payments>
         <PaymentInfo>
           <PaymentInfoID>2</PaymentInfoID>
-          <PaymentAmount>23.2</PaymentAmount>
+          <PaymentAmount>' . $data['total_price'] . '</PaymentAmount>
           <PaymentMethodID>COD</PaymentMethodID>
           <BillingInfo>
-            <FirstName>'.$data['customer']['first_name'].'</FirstName>
-            <LastName>'.$data['customer']['last_name'].'</LastName>
-            <Address1>test</Address1>
-            <Address2>test</Address2>
-            <Address3>test</Address3>
-            <City>'.$data['customer']['email'].'</City>
-            <State>'.$data['email'].'</State>
-            <PostalCode>'.$data['customer']['last_name'].'</PostalCode>
-            <Country>'.$data['customer']['last_name'].'</Country>
-            <Phone>'.$data['customer']['last_name'].'</Phone>
-            <Email>'.$data['customer']['last_name'].'</Email>
+            <FirstName>' . $data['billing_address']['first_name'] . '</FirstName>
+            <LastName>' . $data['billing_address']['last_name'] . '</LastName>
+            <Address1>' . $data['billing_address']['address1'] . '</Address1>
+            <Address2>' . $data['billing_address']['address2'] . '</Address2>
+            <City>' . $data['billing_address']['city'] . '</City>
+            <State>' . $data['billing_address']['province'] . '</State>
+            <PostalCode>' . $data['billing_address']['zip'] . '</PostalCode>
+            <Country>' . $data['billing_address']['country'] . '</Country>
+            <Phone>' . $data['billing_address']['phone'] . '</Phone>
           </BillingInfo>
           <PaymentProperties/>
         </PaymentInfo>
@@ -151,67 +174,25 @@ $prepareData = '<?xml version="1.0" encoding="utf-8"?>
   </SubOrders>
   <OrderProperties/>
   <FileList>
-    <MediaFile>
-      <MediaID>1</MediaID>
-      <UploadBatchID/>
-      <SourceType>URL</SourceType>
-      <MediaFileName>image10.jpg</MediaFileName>
-      <MediaFileType>IMAGE</MediaFileType>
-      <Source>
-        http://webservices.fujifilmesys.com/venus/imagebank/image10.jpg
-      </Source>
-      <RelativeSource/>
-      <Rotation>0</Rotation>
-      <SourceWidth>0</SourceWidth>
-      <SourceHeight>0</SourceHeight>
-      <FileDate>0001-01-01T00:00:00</FileDate>
-      <MediaFileProperties/>
-    </MediaFile>
+     '.$mediaFileTag.'
   </FileList>
   <TemplateList/>
 </OrderManifest>
     </OrderSubmit>
   </soap12:Body>
 </soap12:Envelope>';
+
 ///Auth-key
-  $url = 'https://preview.webservices.fujifilmesys.com/fes.digitalintegrationservices/order/orderservices.asmx';
-  $ch = curl_init();
-  curl_setopt( $ch, CURLOPT_URL, $url );
-  curl_setopt( $ch, CURLOPT_POST, true );
-  curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type: application/soap+xml'));
-  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-  curl_setopt( $ch, CURLOPT_POSTFIELDS, $prepareData );
-  $result = curl_exec($ch);
-  curl_close($ch);
-*/
-
-
-$mail = new PHPMailer;
+$url = 'https://preview.webservices.fujifilmesys.com/fes.digitalintegrationservices/order/orderservices.asmx';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/soap+xml'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $prepareData);
+$result = curl_exec($ch);
+curl_close($ch);
 
 
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-//$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->CharSet = 'UTF-8';
-
-$mail->setFrom("testEmail@kontrast.com", "test");
-
-
-$mail->addAddress('mona.subaih@gmail.com', 'Mona');     // Add a recipient
-
-$mail->isHTML(true);                                  // Set email format to HTML
-$data['success'] = 'false';
-$mail->Subject = "Kontrast :: Contact Form";
-$mail->Body = json_encode($data);
-$mail->AltBody = json_encode($data);
-
-if (!$mail->send()) {
-
-    $data['success'] = false;
-    $data['messageError'] = 'Message could not be sent.' . 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    $data['success'] = true;
-}
-echo json_encode($data);
 
