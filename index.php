76f728346c3f580cb696562d97b38e66f85daa1d
Mona subaih <m.subaih@rozn.org>
2017-04-26 10:12:10 +0300

    no message

--- index.php
+++ index.php
@@ -215,7 +215,8 @@ $prepareData = '<?xml version="1.0" encoding="utf-8"?>
   </soap12:Body>
 </soap12:Envelope>';
 
-
+echo $prepareData;
+die();
 ///Auth-key
 $url = 'https://preview.webservices.fujifilmesys.com/fes.digitalintegrationservices/order/orderservices.asmx';
 $ch = curl_init();
